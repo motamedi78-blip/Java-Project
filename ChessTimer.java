99cae644e2a842c3dab4ff8816d8e30210f8c46f
motamedi78-blip <motamedi78@gmail.com>
2026-02-15 19:55:01 +0100

    Update ChessTimer.java

--- ChessTimer.java
+++ ChessTimer.java
@@ -3,27 +3,26 @@ import java.awt.event.*;
 
 public class ChessTimer {
 
-    private int sekunden;
+    private int seconds;
     private Timer timer;
     private JLabel label;
 
-    public ChessTimer(int sekunden,JLabel label){
-        this.sekunden=sekunden;
-        this.label=label;
+    public ChessTimer(int seconds, JLabel label){
+        this.seconds = seconds;
+        this.label = label;
 
-        timer=new Timer(1000,new ActionListener(){
-            public void actionPerformed(ActionEvent e){
-                sekunden--;
-                label.setText(format());
-                if(sekunden<=0){
-                    timer.stop();
-                    JOptionPane.showMessageDialog(null,"Zeit vorbei!");
-                }
+        timer = new Timer(1000, e -> {
+            seconds--;
+            label.setText(format());
+            if(seconds <= 0){
+                seconds = 0;
+                timer.stop();
+                JOptionPane.showMessageDialog(null,"Time's up!");
             }
         });
     }
 
-    private String format(){ int m=sekunden/60; int s=sekunden%60; return String.format("%02d:%02d",m,s); }
-    public void starten(){ timer.start(); }
-    public void stoppen(){ timer.stop(); }
+    private String format(){ int m=seconds/60; int s=seconds%60; return String.format("%02d:%02d",m,s); }
+    public void start(){ timer.start(); }
+    public void stop(){ timer.stop(); }
 }
