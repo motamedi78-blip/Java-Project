e5841df815a421d5f115cbe73dffa264d736c05d
motamedi78-blip <motamedi78@gmail.com>
2026-02-15 19:55:25 +0100

    Update ChessGUI.java

--- ChessGUI.java
+++ ChessGUI.java
@@ -3,151 +3,122 @@ import java.awt.*;
 
 public class ChessGUI extends JFrame {
 
-    private JButton[][] felder = new JButton[8][8];
-    private Point ausgewaehlt = null;
-
-    private SchachSpiel spiel = new SchachSpiel();
-
-    private JLabel weissLabel = new JLabel("05:00", SwingConstants.CENTER);
-    private JLabel schwarzLabel = new JLabel("05:00", SwingConstants.CENTER);
-
-    private ChessTimer weissTimer;
-    private ChessTimer schwarzTimer;
+    private JButton[][] squares = new JButton[8][8];
+    private Point selected = null;
+    private SchachSpiel game = new SchachSpiel();
+    private JLabel whiteLabel = new JLabel("05:00", SwingConstants.CENTER);
+    private JLabel blackLabel = new JLabel("05:00", SwingConstants.CENTER);
+    private ChessTimer whiteTimer;
+    private ChessTimer blackTimer;
 
     public ChessGUI() {
-
-        setTitle("Schach");
-        setSize(600, 650);
+        setTitle("Chess");
+        setSize(600,650);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLayout(new BorderLayout());
 
-        JPanel oben = new JPanel(new GridLayout(1, 2));
-        oben.add(weissLabel);
-        oben.add(schwarzLabel);
-        add(oben, BorderLayout.NORTH);
+        JPanel top = new JPanel(new GridLayout(1,2));
+        top.add(whiteLabel);
+        top.add(blackLabel);
+        add(top,BorderLayout.NORTH);
 
-        JPanel brettPanel = new JPanel(new GridLayout(8, 8));
-        add(brettPanel, BorderLayout.CENTER);
+        JPanel boardPanel = new JPanel(new GridLayout(8,8));
+        add(boardPanel,BorderLayout.CENTER);
 
-        for (int r = 0; r < 8; r++) {
-            for (int c = 0; c < 8; c++) {
+        for(int r=0;r<8;r++){
+            for(int c=0;c<8;c++){
+                JButton square = new JButton();
+                square.setFocusPainted(false);
+                square.setBorderPainted(false);
+                square.setBackground((r+c)%2==0?Color.WHITE:Color.LIGHT_GRAY);
 
-                JButton feld = new JButton();
-                feld.setFocusPainted(false);
-                feld.setBorderPainted(false);
-                feld.setBackground((r + c) % 2 == 0 ? Color.WHITE : Color.LIGHT_GRAY);
+                int row=r, col=c;
+                square.addActionListener(e->squareClick(row,col));
 
-                int zeile = r;
-                int spalte = c;
-
-                feld.addActionListener(e -> feldKlick(zeile, spalte));
-
-                felder[r][c] = feld;
-                brettPanel.add(feld);
+                squares[r][c]=square;
+                boardPanel.add(square);
             }
         }
 
-        aktualisiereBrett();
+        updateBoard();
 
-        weissTimer = new ChessTimer(300, weissLabel);
-        schwarzTimer = new ChessTimer(300, schwarzLabel);
-
-        weissTimer.starten();
+        whiteTimer=new ChessTimer(300,whiteLabel);
+        blackTimer=new ChessTimer(300,blackLabel);
+        whiteTimer.start();
 
         setLocationRelativeTo(null);
         setVisible(true);
     }
 
-    private void feldKlick(int r, int c) {
+    private void squareClick(int r,int c){
+        String piece=game.getPiece(r,c);
 
-        String figur = spiel.getPiece(r, c);
-
-        if (ausgewaehlt == null) {
-            if (!figur.equals("") && figur.startsWith(spiel.getCurrentTurn())) {
-                ausgewaehlt = new Point(r, c);
-                zeigeMoeglicheZuege(r, c);
+        if(selected==null){
+            if(!piece.equals("") && piece.startsWith(game.getCurrentTurn())){
+                selected=new Point(r,c);
+                showPossibleMoves(r,c);
             }
             return;
         }
 
-        boolean zugErfolgreich = spiel.move(ausgewaehlt.x, ausgewaehlt.y, r, c);
+        boolean moved=game.move(selected.x,selected.y,r,c);
 
-        farbenZuruecksetzen();
-        ausgewaehlt = null;
+        resetColors();
+        selected=null;
 
-        if (zugErfolgreich) {
-            aktualisiereBrett();
-
-            if (spiel.getCurrentTurn().equals("weiss")) {
-                schwarzTimer.stoppen();
-                weissTimer.starten();
+        if(moved){
+            updateBoard();
+            if(game.getCurrentTurn().equals("white")){
+                blackTimer.stop();
+                whiteTimer.start();
             } else {
-                weissTimer.stoppen();
-                schwarzTimer.starten();
+                whiteTimer.stop();
+                blackTimer.start();
             }
         }
     }
 
-    private void zeigeMoeglicheZuege(int r, int c) {
+    private void showPossibleMoves(int r,int c){
+        String piece=game.getPiece(r,c);
+        if(piece.equals("") || !piece.startsWith(game.getCurrentTurn())) return;
 
-        String figur = spiel.getPiece(r, c);
+        String[] parts=piece.split("_");
+        String color=parts[0], type=parts[1];
 
-        if (figur.equals("") || !figur.startsWith(spiel.getCurrentTurn()))
-            return;
+        boolean[][] possible=new boolean[8][8];
 
-        String[] teile = figur.split("_");
-        String farbe = teile[0];
-        String typ = teile[1];
-
-        boolean[][] moeglicheZuege = new boolean[8][8];
-
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
-
-                if (ChessMoveCheck.checkMove(typ, farbe, r, c, i, j, spiel.getBoard())) {
-                    String ziel = spiel.getPiece(i, j);
-                    if (ziel.equals("") || !ziel.startsWith(farbe)) {
-                        moeglicheZuege[i][j] = true;
-                    }
+        for(int i=0;i<8;i++)
+            for(int j=0;j<8;j++)
+                if(ChessMoveCheck.checkMove(type,color,r,c,i,j,game.getBoard())){
+                    String target=game.getPiece(i,j);
+                    if(target.equals("") || !target.startsWith(color)) possible[i][j]=true;
                 }
+
+        for(int i=0;i<8;i++)
+            for(int j=0;j<8;j++)
+                if(possible[i][j]) squares[i][j].setBackground(Color.GREEN);
+
+        squares[r][c].setBackground(Color.YELLOW);
+    }
+
+    private void updateBoard(){
+        for(int r=0;r<8;r++)
+            for(int c=0;c<8;c++){
+                squares[r][c].setIcon(null);
+                String piece=game.getPiece(r,c);
+                if(!piece.equals("")) squares[r][c].setIcon(loadIcon("images/"+piece+".png"));
             }
-        }
-
-        for (int i = 0; i < 8; i++)
-            for (int j = 0; j < 8; j++)
-                if (moeglicheZuege[i][j])
-                    felder[i][j].setBackground(Color.GREEN);
-
-        felder[r][c].setBackground(Color.YELLOW);
     }
 
-    private void aktualisiereBrett() {
-
-        for (int r = 0; r < 8; r++) {
-            for (int c = 0; c < 8; c++) {
-                felder[r][c].setIcon(null);
-
-                String figur = spiel.getPiece(r, c);
-
-                if (!figur.equals("")) {
-                    felder[r][c].setIcon(ladeIcon("images/" + figur + ".png"));
-                }
-            }
-        }
+    private void resetColors(){
+        for(int r=0;r<8;r++)
+            for(int c=0;c<8;c++)
+                squares[r][c].setBackground((r+c)%2==0?Color.WHITE:Color.LIGHT_GRAY);
     }
 
-    private void farbenZuruecksetzen() {
-
-        for (int r = 0; r < 8; r++)
-            for (int c = 0; c < 8; c++)
-              felder[r][c].setBackground((r + c) % 2 == 0 ? Color.WHITE : Color.LIGHT_GRAY);
-    }
-
-    private ImageIcon ladeIcon(String pfad) {
-
-        ImageIcon icon = new ImageIcon(pfad);
-        Image bild = icon.getImage().getScaledInstance(64, 64, Image.SCALE_SMOOTH);
-        return new ImageIcon(bild);
+    private ImageIcon loadIcon(String path){
+        ImageIcon icon=new ImageIcon(path);
+        Image img=icon.getImage().getScaledInstance(64,64,Image.SCALE_SMOOTH);
+        return new ImageIcon(img);
     }
 }
-
